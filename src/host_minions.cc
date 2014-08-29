25d5d3ef180a96b00da92ea8c5a426251da1d1f6
Lee James O'Riordan <lee.oriordan@oist.jp>
2014-08-29 18:24:27 +0900

    Added helper functions in host_minions.cc

---
+++ src/host_minions.cc
@@ -0,0 +1,12 @@
+
+double* recurseForD(double* op, double* grid, int dim);
+
+
+
+
+double* recurseForD(double* (*func)(double*), double* oper, double* grid, int dim, int *gridSize){
+	if( dim ){
+		
+	}
+
+}
