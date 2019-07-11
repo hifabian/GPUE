c199fbb9205e4a6530d48013a1c000592a0338ca
Ben Chislett <chislett.ben@gmail.com>
2019-07-11 13:28:52 +0900

    Update hdf5 build script to write to stdout

--- bin/install_hdf5.sh
+++ bin/install_hdf5.sh
@@ -13,13 +13,21 @@ if [ -d "./$LOCAL_HDF_PATH" ]; then
     exit 0
 fi
 
+echo "Fetching HDF5 from remote"
 wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-$HDF_VER_NO_PATCH/hdf5-$HDF_VER/src/CMake-hdf5-$HDF_VER.tar.gz
+echo "Got HDF5 archive"
 
+echo "Extracting HDF5..."
 tar -xvf "CMake-hdf5-$HDF_VER.tar.gz"
+echo "Extracted HDF5"
 
+echo "Running build script"
 cd "CMake-hdf5-$HDF_VER/"
 bash "./build-unix.sh"
+echo "Build step complete"
 
+echo "Extracting built source"
 tar -xvf "HDF5-$HDF_VER-Linux.tar.gz"
+echo "Extracted source"
 echo "$ROOT_PATH/$LOCAL_HDF_PATH"
 exit 0
