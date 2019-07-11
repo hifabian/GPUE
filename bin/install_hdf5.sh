3473fbf8245936a4791fb22c3181cbb724c16ae0
Ben Chislett <chislett.ben@gmail.com>
2019-07-11 14:56:04 +0900

    Use configure and make for install_hdf5 script

--- bin/install_hdf5.sh
+++ bin/install_hdf5.sh
@@ -1,12 +1,7 @@
 HDF_VER_NO_PATCH="1.10"
 HDF_VER="1.10.5"
 ROOT_PATH="$PWD"
-LOCAL_HDF_PATH="CMake-hdf5-$HDF_VER/HDF5-$HDF_VER-Linux/HDF_Group/HDF5/$HDF_VER/include"
-
-if [[ ! -z "$HDF5_DIR" ]]; then
-    echo "$HDF5_DIR"
-    exit 0
-fi
+LOCAL_HDF_PATH="CMake-hdf5-$HDF_VER/hdf5-$HDF_VER/include"
 
 if [ -d "./$LOCAL_HDF_PATH" ]; then
     echo "$PWD/$LOCAL_HDF_PATH"
@@ -15,19 +10,22 @@ fi
 
 echo "Fetching HDF5 from remote"
 wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-$HDF_VER_NO_PATCH/hdf5-$HDF_VER/src/CMake-hdf5-$HDF_VER.tar.gz
-echo "Got HDF5 archive"
+echo "Got archive"
 
-echo "Extracting HDF5..."
-tar -xvf "CMake-hdf5-$HDF_VER.tar.gz"
-echo "Extracted HDF5"
+echo "Extracting..."
+tar -xvf "CMake-hdf5-$HDF_VER.tar.gz" && rm "CMake-hdf5-$HDF_VER.tar.gz"
+echo "Finished Extraction"
 
-echo "Running build script"
-cd "CMake-hdf5-$HDF_VER/"
-bash "./build-unix.sh"
-echo "Build step complete"
+cd "CMake-hdf5-$HDF_VER/hdf5-$HDF_VER"
 
-echo "Extracting built source"
-tar -xvf "HDF5-$HDF_VER-Linux.tar.gz"
-echo "Extracted source"
+echo "Configuring..."
+./configure --prefix=$(pwd) --enable-cxx
+echo "Finished Configuring"
+
+echo "Building..."
+make || cp bin/libszip-static.a bin/libszip.a # work around apparent bug in their makefile
+make && make install
+echo "Finished Building"
+
 echo "$ROOT_PATH/$LOCAL_HDF_PATH"
 exit 0
